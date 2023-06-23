dbdfa541651919bd15c66d477766cfaa68641f2f
Thein Naing <theinnaing0908@gmail.com>
2023-06-23 15:14:25 +0800

    tested login and logout

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,18 @@
 import { useState} from "react";
+import { useLogin} from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+    //invoke useLogin hook
+  const {login, error, isLoading} = useLogin();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    await login(email, password);
   };
+
   return (
     <form className="login" onSubmit={handleSubmit}>
       <h3>Login</h3>
@@ -25,7 +30,8 @@ const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
-      <button>Log in</button>
+      <button disable={isLoading}>Log in</button>
+      {error && <div className='error'>{error}</div>}
     </form>
   );
 };
